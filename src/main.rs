0f08d53c657b2987938ed67bd6da470b8f872693
cat_in_136 <cat.in.136+github@gmail.com>
2018-04-21 01:47:28 +0900

    introduce file input/output with no processing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
 use std::env;
+use std::io;
+use std::fs;
+use std::io::BufReader;
+use std::io::Read;
+use std::io::BufWriter;
+use std::io::Write;
+
+fn main_decode(filename: &String) -> Result<i8,io::Error> {
+    let fin = BufReader::new(fs::File::open(filename)?);
+    let mut fout = BufWriter::new(fs::File::create("bocu-1.txt")?);
+
+    for b in fin.bytes() {
+        // TODO convert
+        fout.write(&[b?])?;
+    }
+
+    Ok(1)
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,8 +25,13 @@ fn main() {
         unimplemented!("encode not implement yet");
         0
     } else if (args.len() == 3) && (args[1] == "decode") {
-        unimplemented!("decode not implement yet");
-        0
+        match main_decode(&args[2]) {
+            Ok(_) => 0,
+            Err(v) => {
+                eprintln!("Error: {:?}", v);
+                1
+            }
+        }
     } else {
         eprintln!("usage:");
         eprintln!("{} encode <filename>", args[0]);
